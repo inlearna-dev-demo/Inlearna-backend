2c0b22311b524b68ccaf64e8728ca26c236deeb3
Anuj Deol <anujdeol007@gmail.com>
2024-11-25 15:53:50 -0500

    stabilized db connection

--- functions/database/dbconnection .js
+++ functions/database/dbconnection .js
@@ -2,13 +2,21 @@ const mongoose = require("mongoose");
 const {defineString} = require("firebase-functions/params");
 
 const uri = defineString("URI");
-const connectionString = uri.value();
+
+let isConnected = false;
 
 /**
  * Initializes the MongoDB connection.
  * @return {Promise} Resolves when the database connection is established.
  */
 async function initializedb() {
+  if (isConnected) {
+    console.log("Reusing existing MongoDB connection");
+    return Promise.resolve();
+  }
+
+  const connectionString = uri.value();
+
   return new Promise((resolve, reject) => {
     mongoose.connect(connectionString, {
       useNewUrlParser: true,
@@ -16,13 +24,16 @@ async function initializedb() {
     });
 
     const db = mongoose.connection;
+
     db.on("error", (err) => {
       console.error("MongoDB connection error:", err);
+      isConnected = false;
       reject(err);
     });
 
     db.once("open", () => {
       console.log("Connected to the MongoDB database");
+      isConnected = true;
       resolve();
     });
   });
